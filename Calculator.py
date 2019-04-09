ffd1a2548c9ed249e950ff3a72032b5ce24ccead
chmehmood4 <48919614+chmehmood4@users.noreply.github.com>
2019-04-09 21:56:54 +0500

    Added source file

---
+++ Calculator.py
@@ -0,0 +1,55 @@
+#Python BootCamp
+#First Assignment
+#Muhammad Mehmood ul hassan
+
+# Define function (adds two numbers)
+
+def add(x, y):
+    return x + y
+
+
+# define function subtracts two numbers
+
+def subtract(x, y):
+    return x - y
+
+
+# define function multiplies two numbers
+
+def multiply(x, y):
+    return x * y
+
+
+# define function (divides two numbers)
+def divide(x, y):
+    return x / y
+
+
+print("Select Operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+# Take input from the user
+
+choice = input("Enter choice(1/2/3/4: ")
+
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+
+
+else:
+    print("Invalid Input")
